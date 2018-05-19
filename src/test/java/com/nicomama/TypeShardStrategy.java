9ed1d7ec10b64e134ab551b738a4cd04e6dc7cc8
shaozhiwei1989 <shaozhiwei1989@163.com>
2018-05-19 09:26:54 +0800

    完善动态数据源

--- src/test/java/com/nicomama/TypeShardStrategy.java
+++ src/test/java/com/nicomama/TypeShardStrategy.java
@@ -1,22 +1,20 @@
 package com.nicomama;
 
-import com.nicomama.annotation.DataSource;
 import com.nicomama.parser.SqlParam;
 import com.nicomama.strategy.ShardStrategy;
 
 public class TypeShardStrategy implements ShardStrategy {
 
-    @DataSource(name = "abcd")
     @Override
     public String parse(SqlParam sqlParam) {
-      //  Long id = (Long) sqlParam.get("id");
+        //  Long id = (Long) sqlParam.get("id");
         return "_" + 5 % 3;//返回分表的后缀
     }
 
     public static void main(String[] args) {
         ShardStrategy strategy = new TypeShardStrategy();
-      String strs=  strategy.parse(null);
-        System.out.printf(strs+"\n");
+        String strs = strategy.parse(null);
+        System.out.printf(strs + "\n");
     }
 
 }
